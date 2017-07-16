720fc3227de38c86ec4b20ebd57629869ce725ea
DESKTOP-IDRETTO\lada_hard <l.gordienkova@gmail.com>
2017-07-16 23:40:32 +0300

    add ful project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            for (int i = 0; i < 101; i++)
+            {
+                if (i % 3 == 0 && i != 0)
+                {
+                    counter++;
+                    Console.WriteLine(3 * counter);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
